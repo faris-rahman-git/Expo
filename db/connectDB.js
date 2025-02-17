f718a70fc6b2deaefe1a5e2719e4680649e42351
faris-rahman-git <farisrahman786687@gmail.com>
2025-02-17 13:44:49 +0530

    Last db and .gitigore changes

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/user',{})
-        console.log('mongoDB connected')
-    }catch(err){
-        console.error(err)
-        process.exit(1)
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {});
+    console.log("mongoDB connected");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
-}
-
-module.exports = connectDB
+module.exports = connectDB;
